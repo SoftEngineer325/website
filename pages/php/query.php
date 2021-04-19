8892c43bbb44415068639ed10b5863cba97daba2
Rohan More <rohan.more.clientnode@gmail.com>
2021-04-19 19:06:14 -0400

    Minor updates, cleaner code

--- pages/php/query.php
+++ pages/php/query.php
@@ -1,33 +1,82 @@
 <?php
+//Get database credentials
 require 'dbc.php';
 
-//Get Highest ranked professor of a course
-//SELECT p.professor_id, p.f_name, p.l_name, p.rank, t.department, t.course_id FROM professors p, teaches t, professors pp WHERE p.professor_id = t.professor_id AND p.rank >= pp.rank GROUP BY (course_id);
-
-if (isset($_GET["best_professors"])) {
-	$columns = array(
-		0 =>'department',
-		1 => 'course_id',
-		2 => 'f_name',
-		3 => 'l_name',
-		4 => 'rank'
+//check to see if a query is posted to the api
+if (isset($_GET["query"])) {
+	//Initizile data variable
+	$data;
+	//switch based on what query is needed
+	switch($_GET["query"]) {
+		case "best_professors":
+			$data = best_professors($conn);
+			break;
+		case "all_courses":
+			$data = all_courses($conn);
+			break;
+		case "electives":
+			$data = get_electives($conn);
+			break;
+		case "get_books":
+			$data = get_books($conn);
+			break;
+		case "meal_plan":
+			$data = get_meal_plans($conn);
+			break;
+		default:
+			$data = array();
+			break;
+	}
+	//organize the data returned from a query
+	$json_data = array(
+		"recordsTotal"    => intval(sizeof($data)),  // total number of records
+		"data"            => $data   // total data array
 	);
+	
+	//Using heading application/json so DataTables and other frameworks know it only contains json data
+	header('Content-Type: application/json');
+	//encode the json data and display it, using prety print to easily read data from a user perspective
+	echo json_encode($json_data, JSON_PRETTY_PRINT);  // send data as json format
 
+//if its not a query, treat it like a normal website
+} else {
+echo 
+"<style>
+	* {
+		background-color: #c00030;
+	}
+	
+	div {
+		color: white;
+		text-align: center;
+		font-weight: bold;
+		position: absolute;
+		top:  50%;
+		left: 50%;
+		transform: translate(-50%,-50%);
+	}
+</style>
+
+<div>
+	<h1><u>Website API</u></h1>
+	<p>Made for a Software Eningeering project at Frostburg State University</p>
+	<br>
+	<p>Made By</p>
+	<p>Rohan More, Megan Murphy, Nathalie Nyanga, David Morton</p>
+	<img src='../../images/paw.png'>
+</div>";
+}
+
+/**
+ * Function that returns the best professors to every course in our database
+ * Courses are from all majors/minors
+ */
+function best_professors($conn) {
 	$sql = "SELECT p.professor_id, p.rank, p.f_name, p.l_name, t.department, t.course_id 
 			FROM professors p, teaches t, professors pp 
 			WHERE p.professor_id = t.professor_id AND p.rank >= pp.rank ". //AND t.department = $_GET[best_professors]
 			"GROUP BY course_id, department";
-
-	$query=mysqli_query($conn, $sql) or die("test.php: get courses");
-	$totalData = mysqli_num_rows($query);
-	$totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
-
-	/*
-	$sql = "SELECT * FROM professors INNER JOIN teaches ON professors.professor_id = teaches.professor_id";
-	$query=mysqli_query($conn, $sql) or die("test.php: get courses");
-	$totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result without limit in the query
-	$query=mysqli_query($conn, $sql) or die("test.php: get courses"); // again run query with limit
-	*/
+	$query = mysqli_query($conn, $sql) or die("query.php: get best professors");
 
 	$data = array();
 	while($row = mysqli_fetch_array($query)) {  // preparing an array
@@ -40,39 +89,16 @@ if (isset($_GET["best_professors"])) {
 
 		$data[] = $nestedData;
 	}
-	$json_data = array(
-				"recordsTotal"    => intval( $totalData ),  // total number of records
-				//"recordsFiltered" => intval( $totalFiltered ), // total number of records after searching, if there is no searching then totalFiltered = totalData
-				"data"            => $data   // total data array
-				);
+	return $data;
+}
 
-	header('Content-Type: application/json');
-	echo json_encode($json_data, JSON_PRETTY_PRINT);  // send data as json format
-
-} 
-else if (isset($_GET["all_courses"])) {
-
-	//$requestData= $_REQUEST;
-	$columns = array(
-	// datatable column index  => database column name
-		0 =>'department',
-		1 => 'course_id',
-		2 => 'f_name',
-		3 => 'l_name',
-		4 => 'rank'
-	);
-	// getting total number records without any search
+/**
+ * Function that returns all the courses and every professor who teaches them
+ * Courses are for all majors/minors
+ */
+function all_courses($conn) {
 	$sql = "SELECT * FROM professors INNER JOIN teaches ON professors.professor_id = teaches.professor_id";
-	$query=mysqli_query($conn, $sql) or die("test.php: get courses");
-	$totalData = mysqli_num_rows($query);
-	$totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
-	
-	/*
-	$sql = "SELECT * FROM professors INNER JOIN teaches ON professors.professor_id = teaches.professor_id";
-	$query=mysqli_query($conn, $sql) or die("test.php: get courses");
-	$totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result without limit in the query
-	$query=mysqli_query($conn, $sql) or die("test.php: get courses"); // again run query with limit
-	*/
+	$query = mysqli_query($conn, $sql) or die("query.php: get all courses");
 	
 	$data = array();
 	while($row = mysqli_fetch_array($query)) {  // preparing an array
@@ -85,53 +111,67 @@ else if (isset($_GET["all_courses"])) {
 	
 		$data[] = $nestedData;
 	}
-	$json_data = array(
-				"recordsTotal"    => intval( $totalData ),  // total number of records
-				//"recordsFiltered" => intval( $totalFiltered ), // total number of records after searching, if there is no searching then totalFiltered = totalData
-				"data"            => $data   // total data array
-				);
-	
-	header('Content-Type: application/json');
-	echo json_encode($json_data, JSON_PRETTY_PRINT);  // send data as json format
-} 
-else if (isset($_GET["courses"])) { 
-		//$requestData= $_REQUEST;
-		$columns = array(
-			// datatable column index  => database column name
-				//0 =>'department',
-				0 => 'course_name',
-				1 => 'course_id',
-				2 => 'description',
-				3 => "offered",
-				4 => "prerequisites"
-			);
-			// getting total number records without any search
-			$sql = "SELECT * FROM courses WHERE department = '".$_GET["courses"]."'";
-			$query=mysqli_query($conn, $sql) or die("test.php: get courses");
-			$totalData = mysqli_num_rows($query);
-			$totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
-			
-			$data = array();
-			while($row = mysqli_fetch_array($query)) {  // preparing an array
-				$nestedData = array();
-				//$nestedData[] = $row["department"];
-				$nestedData[] = $row["course_name"];
-				$nestedData[] = $row["course_id"];
-				$nestedData[] = $row["description"];
-				$nestedData[] = $row["offered"];
-				$nestedData[] = $row["prerequisites"];
+	return $data;
+}
 
-				$data[] = $nestedData;
-			}
-			$json_data = array(
-						"recordsTotal"    => intval( $totalData ),  // total number of records
-						//"recordsFiltered" => intval( $totalFiltered ), // total number of records after searching, if there is no searching then totalFiltered = totalData
-						"data"            => $data   // total data array
-						);
-			
-			header('Content-Type: application/json');
-			echo json_encode($json_data, JSON_PRETTY_PRINT);  // send data as json format
-} else {
-	echo 'Stop snooping around...';
+/**
+ * Function that returns the best electives for a certain minor
+ */
+function get_electives($conn) {
+	$sql = "SELECT * FROM courses WHERE best_for LIKE '%".$_GET["major"]."%'";
+	$query = mysqli_query($conn, $sql) or die("query.php: get electives for major");
+	
+	$data = array();
+	while($row = mysqli_fetch_array($query)) {  // preparing an array
+		$nestedData = array();
+		$nestedData[] = $row["department"];
+		$nestedData[] = $row["course_name"];
+		$nestedData[] = $row["course_id"];
+		$nestedData[] = $row["description"];
+		$nestedData[] = $row["offered"];
+		$nestedData[] = $row["prerequisites"];
+
+		$data[] = $nestedData;
+	}
+	return $data;
+}
+
+/**
+ * Function that returns a set of books based on isbn type and numbers
+ */
+function get_books($conn) {
+	$data = array();
+	$sql = ($_GET["isbn"] == 10 ? "SELECT * FROM books WHERE `isbn-10` IN (".$_GET["books"].")" : 
+								  "SELECT * FROM books WHERE `isbn-13` IN (".$_GET["books"].")" );
+	$query = mysqli_query($conn, $sql) or die("query.php: get books for isbn");
+		
+	while($row = mysqli_fetch_array($query)) {  // preparing an array
+		$nestedData = array();
+		$nestedData[] = ($_GET["isbn"] == 10 ? $row["isbn-10"] : $row["isbn-13"]);
+		$nestedData[] = $row["book_name"];
+		$nestedData[] = $row["price"];
+		$nestedData[] = $row["company"];
+
+		$data[] = $nestedData;
+	}
+	return $data;
+}
+
+function get_meal_plans($conn) {
+	$sql = "SELECT * WHERE student_type LIKE '%".$_GET["plan"]."%'";
+	$query = mysqli_query($conn, $sql) or die("query.php: get electives for major");
+	
+	$data = array();
+	while($row = mysqli_fetch_array($query)) {  // preparing an array
+		$nestedData = array();
+		$nestedData[] = $row["student_type"];
+		$nestedData[] = $row["meal_plan_name"];
+		$nestedData[] = $row["description"];
+		$nestedData[] = $row["description"];
+		$nestedData[] = $row["cost"];
+
+		$data[] = $nestedData;
+	}
+	return $data;
 }
 ?>
--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,123 @@
 window.onload = function () {
-
-var courses = ["COSC", "ITEC", "SCIA"];
-var dropdown = document.getElementById('courses');
-  
-//Loop through json and append it to the table
-courses.forEach(function(object) {
-    var option = document.createElement('option');
-    option.innerHTML = '<option value="' + object + '">' + object + '</option>';
-    dropdown.appendChild(option);
-});
-
   console.info(window.location.pathname);
-  if (location.pathname == "/website/pages/classadvise.html") {
-      //DataTables can query the site for us and fill the table!
-        $('#table1').DataTable({
-          'processing': true,
-          'language': {
-            'loadingRecords': 'Attempting to load records...',
-            'processing': 'Loaded records, displaying...'
-        },
-          "pagingType": "full_numbers",
-          "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, 20]],
-          "order": [[ 1, "asc" ]],
-          "ajax": {url:"php/query.php?all_courses", type: "post"}
-        });
-        $('#table2').DataTable({
-          'processing': true,
-          "ordering": false,
-          'language': {
-            'loadingRecords': 'Attempting to load records...',
-            'processing': 'Loaded records, displaying...',
-            "emptyTable": "Please select a major to show best electives!"
-        },
-          "pagingType": "full_numbers",
-          "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, 20]],
-          //"order": [[ 1, "asc" ]],
-        });
-        $('.dataTables_length').addClass('bs-select');
-        //Disable message box displaying when there is an error
-        $.fn.dataTable.ext.errMode = 'none';
+  if (location.pathname == "/pages/classadvise.html") {
+    //load dropdown menu items for majors
+    var courses = ["COSC", "CIS", "ITEC", "SCIA"];
+    var dropdown = document.getElementById('courses');
+      
+    //Loop through array and append it to the drop down menu
+    courses.forEach(function(object) {
+        var option = document.createElement('option');
+        option.innerHTML = '<option value="' + object + '">' + object + '</option>';
+        dropdown.appendChild(option);
+    });
+  }
+
+  if (location.pathname == "/pages/moneymanagement.html") {
+
+    //load dropdown menu items for majors
+    var student_types = ["Residental", "Commuter & Edgewood"];
+    var dropdown_student_type = document.getElementById('studenttype');
+      
+    //Loop through array and append it to the drop down menu
+    student_types.forEach(function(object) {
+      var option = document.createElement('option');
+      option.innerHTML = '<option value="' + object + '">' + object + '</option>';
+      dropdown_student_type.appendChild(option);
+    });
+
+    $('#book_table').DataTable({
+      'processing': true,
+      "ordering": false,
+      'language': {
+        'loadingRecords': 'Attempting to load records...',
+        'processing': 'Loaded records, displaying...',
+        "emptyTable": "Waiting for your input!"
+    },
+      "searching": false,
+      "paging": false,
+      "pagingType": "full_numbers",
+      "lengthMenu": [[4], [4]],
+      //"order": [[ 1, "asc" ]],
+    });
+
+    $('#meal_plans').DataTable({
+      'processing': true,
+      "ordering": false,
+      'language': {
+        'loadingRecords': 'Attempting to load records...',
+        'processing': 'Loaded records, displaying...',
+        "emptyTable": "Waiting for your input!"
+    },
+      "searching": false,
+      "paging": false,
+      "pagingType": "full_numbers",
+      "lengthMenu": [[4], [4]],
+      //"order": [[ 1, "asc" ]],
+    });
   }
 }
 
+$(document).ready(function() {
+      //DataTables can query the site for us and fill the table!
+      //Table1 = All Courses
+      $('#table1').DataTable({
+        'processing': true,
+        'language': {
+          'loadingRecords': 'Attempting to load records...',
+          'processing': 'Loaded records, displaying...'
+      },
+        "pagingType": "full_numbers",
+        "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, 20]],
+        "order": [[ 1, "asc" ]],
+        "ajax": {url:"php/query.php?query=all_courses", type: "post"}
+      });
+
+      //Table2 = Major Electives
+      $('#table2').DataTable({
+        'processing': true,
+        "ordering": false,
+        'language': {
+          'loadingRecords': 'Attempting to load records...',
+          'processing': 'Loaded records, displaying...',
+          "emptyTable": "Please select a major to show best electives!"
+      },
+        "pagingType": "full_numbers",
+        "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, 20]],
+        //"order": [[ 1, "asc" ]],
+      });
+      $('.dataTables_length').addClass('bs-select');
+      //Disable message box displaying when there is an error
+      $.fn.dataTable.ext.errMode = 'none';
+});
+
+//Get meal plans
+$("#studenttype").change(function() {
+  //Get what select the user picks
+  var userRequest = $("#studenttype").val();
+  console.log(userRequest);
+
+  if (userRequest) {
+      //Destroy the table so we can create a new one
+    $('#meal_plans').DataTable().destroy();
+    $('#meal_plans').DataTable({
+      'processing': true,
+      "ordering": false,
+      'language': {
+        'loadingRecords': 'Attempting to load records...',
+        'processing': 'Loaded records, displaying...',
+        "emptyTable": "Sorry, we couldn't find any meal plans!"
+    },
+      "pagingType": "full_numbers",
+      "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, 20]],
+      //"order": [[ 1, "asc" ]],
+      "ajax": {url:"php/query.php?query=meal_plan&plan=" + userRequest, type: "post"}
+    });
+    $('.dataTables_length').addClass('bs-select');
+  }
+});
+
+//Get best electives for major
 $("#courses").change(function() {
     //Get what select the user picks
     var userRequest = $("#courses").val();
@@ -61,13 +137,13 @@ $("#courses").change(function() {
         "pagingType": "full_numbers",
         "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, 20]],
         //"order": [[ 1, "asc" ]],
-        "ajax": {url:"php/query.php?courses=" + userRequest, type: "post"}
+        "ajax": {url:"php/query.php?query=electives&major=" + userRequest, type: "post"}
       });
       $('.dataTables_length').addClass('bs-select');
     }
-
 });
 
+//Display either all professors or the best ones for majors
 $("#change_table").change(function() {
   //Destroy the table so we can create a new one
   $('#table1').DataTable().destroy();
@@ -79,10 +155,101 @@ $("#change_table").change(function() {
     "pagingType": "full_numbers",
     "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, 20]],
     "order": [[ 1, "asc" ]],
-    "ajax": {url:"php/query.php?" + userRequest, type: "post"}
+    "ajax": {url:"php/query.php?query=" + userRequest, type: "post"}
   });
   $('.dataTables_length').addClass('bs-select');
   //Disable message box displaying when there is an error
   $.fn.dataTable.ext.errMode = 'none';
 });
 
+
+var maxChars;
+$("#isbn13").click(function() {
+  document.getElementById("clearInput").click();
+  document.getElementById("books").cols = "14";
+  maxChars = document.getElementById("books").cols;
+});
+
+$("#isbn10").click(function() {
+  document.getElementById("clearInput").click();
+  document.getElementById("books").cols = "13";
+  maxChars = document.getElementById("books").cols;
+});
+
+$("#clearInput").click(function() {
+  document.getElementById("books").value = "";
+});
+
+//TextArea Limits
+$(document).ready(function(){
+  var textArea = $('#books');
+  var maxRows = textArea.attr('rows');
+  maxChars = textArea.attr('cols');
+  textArea.keypress(function(e){
+      var text = textArea.val();
+      var lines = text.split('\n');
+      if (e.keyCode == 13){
+          return lines.length < maxRows;
+      }
+      //Should check for backspace/del/etc.
+      else{ 
+          var caret = textArea.get(0).selectionStart;
+          
+          var line = 0;
+          var charCount = 0;
+          $.each(lines, function(i,e){
+              charCount += e.length;
+              if (caret <= charCount){
+                  line = i;
+                  return false;
+              }
+              //\n count for 1 char;
+              charCount += 1;
+          });
+                
+          var theLine = lines[line];
+          return theLine.length < maxChars;
+      }
+  });
+});
+
+function isNumberKey(evt) {
+  var charCode = (evt.which) ? evt.which : event.keyCode
+  if (charCode != 45  && charCode > 31 && (charCode < 48 || charCode > 57))
+      return false;
+
+  return true;
+}
+
+//Display Books
+$("#getBooks").click(function() {
+  var data = document.getElementById("books");
+  var books = data.value.replace(/\r\n/g,"\n").split("\n").filter(Boolean);
+  if (books.length == 0) {
+    return false;
+  }
+  books = "'" + books.join("','") + "'";
+  var method = $("input[type='radio'][name='isbnRadio']:checked").val();
+
+  console.log(method + ": " + books);
+  //Destroy the table so we can create a new one
+  $('#book_table').DataTable().destroy();
+  //Create a new table with the users request
+  $('#book_table').DataTable({
+    'processing': true,
+    'language': {
+      'loadingRecords': 'Attempting to load records...',
+      'processing': 'Loaded records, displaying...',
+      "emptyTable": "Your books were not found!"
+  },
+    "searching": false,
+    "paging": false,
+    "pagingType": "full_numbers",
+    "lengthMenu": [[4], [4]],
+    "order": [[ 2, "asc" ]],
+    "ajax": {url:"php/query.php?query=get_books&isbn=" + method + "&books=" + books, type: "post"}
+  });
+  $('.dataTables_length').addClass('bs-select');
+  //Disable message box displaying when there is an error
+  $.fn.dataTable.ext.errMode = 'none';
+});
